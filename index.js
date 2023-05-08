b2532cf7ccfe5e270e20a5536eae8993d36367db
vladimir levadny <vladimir.levadny@gmail.com>
2023-05-09 02:36:48 +0300

    Add server

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { usersRouter } from "./users/users.js"
+
+const port = 8000;
+const app = express();
+
+app.get("/hello", (req, res) => {
+	res.send("Hello");
+	res.end();
+});
+
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+	console.log(`Сервер запущен на http://localhost:${port}`)
+});
